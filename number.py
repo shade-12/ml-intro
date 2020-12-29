bf7d6ed7dfdb0dbb2d99a348acce3b50cca3f4ed
Shade Wong <shadeying@alumni.ubc.ca>
2020-12-28 19:38:57 -0800

    Done python intro

--- number.py
+++ number.py
@@ -1,1 +1,10 @@
+import math
+
 the_number = 2020
+
+def quote_marvin():
+    print("I've calculated your chance of survival, but I don't think you'll like it.")
+    
+def square_root(num):
+    return math.sqrt(num)
+    
